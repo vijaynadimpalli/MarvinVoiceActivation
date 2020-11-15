d73bb23c40af066bbc0d038fb7c367752ba8997d
vijaynadimpalli <vijay.nadimpalli@gmail.com>
2020-11-15 18:23:12 +0530

    Update PycharmInferenceHelper.py

--- Pycharm_Inference/PycharmInferenceHelper.py
+++ Pycharm_Inference/PycharmInferenceHelper.py
@@ -26,17 +26,11 @@ def chime_on_activate(filename, chime_file, predictions, threshold):
     audio_clip = AudioSegment.from_wav(filename)
     chime = AudioSegment.from_wav(chime_file)
     Ty = predictions.shape[1]
-    # Step 1: Initialize the number of consecutive output steps to 0
     consecutive_timesteps = 0
-    # Step 2: Loop over the output steps in the y
     for i in range(Ty):
-        # Step 3: Increment consecutive output steps
         consecutive_timesteps += 1
-        # Step 4: If prediction is higher than the threshold and more than 75 consecutive output steps have passed
         if predictions[0, i, 0] > threshold and consecutive_timesteps > 75:
-            # Step 5: Superpose audio and background using pydub
             audio_clip = audio_clip.overlay(chime, position=((i / Ty) * audio_clip.duration_seconds) * 1000)
-            # Step 6: Reset consecutive output steps to 0
             consecutive_timesteps = 0
 
     audio_clip.export("chime_output.wav", format='wav')
