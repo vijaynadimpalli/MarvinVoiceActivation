2150683765ace196eb5d7ca1ab9c67e4bcf51091
vijaynadimpalli <vijay.nadimpalli@gmail.com>
2020-11-15 18:21:25 +0530

    Update DataCreator.py

--- build/DataCreator.py
+++ build/DataCreator.py
@@ -112,18 +112,11 @@ def load_raw_audio(silent_background=False):
 def get_random_time_segment(segment_ms):
     """
     Gets a random time segment of duration segment_ms in a 10,000 ms audio clip.
-
-    Arguments:
-    segment_ms -- the duration of the audio clip in ms ("ms" stands for "milliseconds")
-
-    Returns:
-    segment_time -- a tuple of (segment_start, segment_end) in ms
     """
 
     segment_start = np.random.randint(low=0,
                                       high=10000 - segment_ms)  # Make sure segment doesn't run past the 10sec background
     segment_end = segment_start + segment_ms - 1
-
     return (segment_start, segment_end)
 
 
@@ -131,28 +124,12 @@ def get_random_time_segment(segment_ms):
 def is_overlapping(segment_time, previous_segments):
     """
     Checks if the time of a segment overlaps with the times of existing segments.
-
-    Arguments:
-    segment_time -- a tuple of (segment_start, segment_end) for the new segment
-    previous_segments -- a list of tuples of (segment_start, segment_end) for the existing segments
-
-    Returns:
-    True if the time segment overlaps with any of the existing segments, False otherwise
     """
-
     segment_start, segment_end = segment_time
-
-    ### START CODE HERE ### (≈ 4 lines)
-    # Step 1: Initialize overlap as a "False" flag. (≈ 1 line)
     overlap = False
-
-    # Step 2: loop over the previous_segments start and end times.
-    # Compare start/end times and set the flag to True if there is an overlap (≈ 3 lines)
     for previous_start, previous_end in previous_segments:
         if segment_start <= previous_end and segment_end >= previous_start:
             overlap = overlap | True
-    ### END CODE HERE ###
-
     return overlap
 
 
@@ -160,36 +137,13 @@ def insert_audio_clip(background, audio_clip, previous_segments):
     """
     Insert a new audio segment over the background noise at a random time step, ensuring that the
     audio segment does not overlap with existing segments.
-
-    Arguments:
-    background -- a 10 second background audio recording.
-    audio_clip -- the audio clip to be inserted/overlaid.
-    previous_segments -- times where audio segments have already been placed
-
-    Returns:
-    new_background -- the updated background audio
     """
-
-    # Get the duration of the audio clip in ms
     segment_ms = len(audio_clip)
-
-    ### START CODE HERE ###
-    # Step 1: Use one of the helper functions to pick a random time segment onto which to insert
-    # the new audio clip. (≈ 1 line)
     segment_time = get_random_time_segment(segment_ms)
-
-    # Step 2: Check if the new segment_time overlaps with one of the previous_segments. If so, keep
-    # picking new segment_time at random until it doesn't overlap. (≈ 2 lines)
     while is_overlapping(segment_time, previous_segments):
         segment_time = get_random_time_segment(segment_ms)
-
-    # Step 3: Append the new segment_time to the list of previous_segments (≈ 1 line)
     previous_segments.append(segment_time)
-    ### END CODE HERE ###
-
-    # Step 4: Superpose audio segment and background
     new_background = background.overlay(audio_clip, position=segment_time[0])
-
     return new_background, segment_time
 
 
@@ -199,41 +153,19 @@ def insert_ones(y, segment_end_ms):
     Update the label vector y. The labels of the 50 output steps strictly after the end of the segment
     should be set to 1. By strictly we mean that the label of segment_end_y should be 0 while, the
     50 following labels should be ones.
-
-
-    Arguments:
-    y -- numpy array of shape (1, Ty), the labels of the training example
-    segment_end_ms -- the end time of the segment in ms
-
-    Returns:
-    y -- updated labels
     """
 
     # duration of the background (in terms of spectrogram time-steps)
     segment_end_y = int(segment_end_ms * Ty / 10000.0)
-
-    # Add 1 to the correct index in the background label (y)
-    ### START CODE HERE ### (≈ 3 lines)
     for i in range(segment_end_y + 1, segment_end_y + 51):
         if i < y.shape[1]:
             y[0, i] = 1
-    ### END CODE HERE ###
-
     return y
 
 
 def create_training_example(backgrounds, activates, negatives):
     """
     Creates a training example with a given background, activates, and negatives.
-
-    Arguments:
-    background -- a 10 second background audio recording
-    activates -- a list of audio segments of the word "activate"
-    negatives -- a list of audio segments of random words that are not "activate"
-
-    Returns:
-    x -- the spectrogram of the training example
-    y -- the label at each time step of the spectrogram
     """
 
     # Set the random seed
@@ -249,22 +181,12 @@ def create_training_example(backgrounds, activates, negatives):
 
 
     background = background - 30
-
-    ### START CODE HERE ###
-    # Step 1: Initialize y (label vector) of zeros (≈ 1 line)
     y = np.zeros((1, Ty))
-
-    # Step 2: Initialize segment times as an empty list (≈ 1 line)
     previous_segments = []
-    ### END CODE HERE ###
-
-    # Select 0-4 random "activate" audio clips from the entire list of "activates" recordings
     number_of_activates = np.random.randint(0, 5)
     random_indices = np.random.randint(len(activates), size=number_of_activates)
     random_activates = [activates[i] for i in random_indices]
-
-    ### START CODE HERE ### (≈ 3 lines)
-    # Step 3: Loop over randomly selected "activate" clips and insert in background
+    
     for random_activate in random_activates:
         # Insert the audio clip on the background
         background, segment_time = insert_audio_clip(background, random_activate, previous_segments)
@@ -272,20 +194,13 @@ def create_training_example(backgrounds, activates, negatives):
         segment_start, segment_end = segment_time
         # Insert labels in "y"
         y = insert_ones(y, segment_end)
-    ### END CODE HERE ###
-
-    # Select 0-2 random negatives audio recordings from the entire list of "negatives" recordings
     number_of_negatives = np.random.randint(0, 3)
     random_indices = np.random.randint(len(negatives), size=number_of_negatives)
     random_negatives = [negatives[i] for i in random_indices]
-
-    ### START CODE HERE ### (≈ 2 lines)
-    # Step 4: Loop over randomly selected negative clips and insert in background
     for random_negative in random_negatives:
         # Insert the audio clip on the background
         background, _ = insert_audio_clip(background, random_negative, previous_segments)
-    ### END CODE HERE ###
-
+        
     # Standardize the volume of the audio clip
     background = match_target_amplitude(background, -20.0)
     file_handle = background.export("train" + ".wav", format="wav")
